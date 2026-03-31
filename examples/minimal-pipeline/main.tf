be94dad2a91f8a81e497b81225b35ae08350ad34
Jared Holgate <jaredholgate@microsoft.com>
2026-03-31 12:44:20 +0100

    feat: add minimal-pipeline example with single write identity and custom pipeline

---
+++ examples/minimal-pipeline/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_version = "~> 1.9"
+
+  required_providers {
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = "~> 1.7"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Minimal example: single write identity, public agents, no template repo, custom pipeline
+module "test" {
+  source = "../../"
+
+  location                     = var.location
+  azuredevops_organization_name = var.azuredevops_organization_name
+  deployment_mode              = "other"
+  agent_use_self_hosted        = false
+
+  azuredevops_existing_template_repository_name = "not-used"
+
+  azuredevops_pipeline_folder_path = "pipelines"
+  azuredevops_pipelines = {
+    info = {
+      main_file     = "info.yaml"
+      template_path = "info-template.yaml"
+    }
+  }
+
+  environments = {
+    dev = {
+      display_order = 1
+      display_name  = "Development"
+      identities = {
+        read = { enabled = false }
+      }
+    }
+  }
+}
